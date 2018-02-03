dab6c85e04c18615bbe644bf2e8a39d80b11dd4e
Anna Royer <anna.royer@colorado.edu>
2018-02-03 11:51:23 -0700

    Fix name error in all articles spec

--- spec/features/user_sees_all_articles_spec.rb
+++ spec/features/user_sees_all_articles_spec.rb
@@ -4,7 +4,7 @@ describe 'As a user' do
   context 'when I visit /articles' do
     it 'I can see all articles' do
       article_1 = Article.create!(title: "Title 1", body: "Body 1")
-      article_2s = Article.create!(title: "Title 2s", body: "Body 2s")
+      article_2 = Article.create!(title: "Title 2s", body: "Body 2s")
 
       visit '/articles'
 
