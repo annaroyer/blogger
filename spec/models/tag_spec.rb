0ab9534a0aa7091c0763cd51383828dbb5b60eab
Anna Royer <anna.royer@colorado.edu>
2018-02-03 19:44:23 -0700

    Add test for instance_method to_s

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -5,4 +5,14 @@ describe Tag, type: :model do
     it { should have_many(:taggings) }
     it { should have_many(:articles).through(:taggings) }
   end
+
+  describe 'instance methods' do
+    describe '#to_s' do
+      it 'returns the tag name' do
+        tag = Tag.new(name: 'nature')
+
+        expect(tag.to_s).to eq(tag.name)
+      end
+    end
+  end
 end
